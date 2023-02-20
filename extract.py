894ed8311b53396d64fa36b74dd51118d08c6cd1
0ckh4m <ockham@millennio.cc>
2023-02-20 14:40:57 -0300

    First commit

---
+++ extract.py
@@ -0,0 +1,63 @@
+"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+
+The `load_neos` function extracts NEO data from a CSV file, formatted as
+described in the project instructions, into a collection of `NearEarthObject`s.
+
+The `load_approaches` function extracts close approach data from a JSON file,
+formatted as described in the project instructions, into a collection of
+`CloseApproach` objects.
+
+The main module calls these functions with the arguments provided at the command
+line, and uses the resulting collections to build an `NEODatabase`.
+
+You'll edit this file in Task 2.
+"""
+import csv
+import json
+
+from models import NearEarthObject, CloseApproach
+
+
+def load_neos(neo_csv_path = "data/neos.csv"):
+    """Read near-Earth object information from a CSV file.
+
+    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :return: A collection of `NearEarthObject`s.
+    """
+    # TODO: Load NEO data from the given CSV file.
+    neos_collection = []
+    with open(neo_csv_path) as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            # designation - 3
+            # name - 4
+            # diameter - 15
+            # hazardous - 7
+            # info -> not needed
+            neos_collection.append(NearEarthObject(row[3], row[4], row[15], row[7]))
+            # if this doesn't work I must test checking the condition for each parameter, if they exit or not
+    return neos_collection
+
+
+def load_approaches(cad_json_path = "data/cad.json"):
+    """Read close approach data from a JSON file.
+
+    :param cad_json_path: A path to a JSON file containing data about close approaches.
+    :return: A collection of `CloseApproach`es.
+    """
+    # TODO: Load close approach data from the given JSON file.
+    ca_collection = []
+    with open(cad_json_path) as file:
+        contents = json.load(file)
+        for entry in contents["data"]:
+            designation = entry[0]
+            time = entry[3]
+            distance = entry[4]
+            velocity = entry[7]
+            # print(CloseApproach(designation, time, distance, velocity)) # just for my own testing
+            ca_collection.append(CloseApproach(designation, time, distance, velocity))
+        
+    return ca_collection
+
+
